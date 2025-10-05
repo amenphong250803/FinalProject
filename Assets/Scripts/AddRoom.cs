b51cca30c97628bfe27b02b7a67d2b8ee1a5208c
TKhoi <khoicoi2015@gmail.com>
2025-10-05 14:54:55 +0700

    Update room

--- Assets/Scripts/AddRoom.cs
+++ Assets/Scripts/AddRoom.cs
@@ -4,12 +4,7 @@ using UnityEngine;
 
 public class AddRoom : MonoBehaviour
 {
-    private RoomTemplates templates;
+    
 
-    void Start()
-    {
-        templates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplates>();
-        if (!templates.rooms.Contains(this.gameObject))
-            templates.rooms.Add(this.gameObject);
-    }
+    
 }
