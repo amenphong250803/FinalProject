791e7508a31f7c54a403b284ca2562bc0aef2771
Phong <139188133+amenphong250803@users.noreply.github.com>
2025-11-29 08:39:00 +0700

    update dash

--- Assets/Scripts/Player/PlayerStates/Player_DashState.cs
+++ Assets/Scripts/Player/PlayerStates/Player_DashState.cs
@@ -13,7 +13,7 @@ public class Player_DashState : PlayerState
     public override void Enter()
     {
         base.Enter();
-
+        player.lastDashTime = Time.time;
         player.PlayDashSFX();
 
         if (health != null)
